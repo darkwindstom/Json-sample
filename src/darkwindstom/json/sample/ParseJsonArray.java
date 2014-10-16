39341fdfc5f6fc89bc515630ce0340180d7b0397
darkwindstom <darkwindstom@gmail.com>
2014-10-16 21:26:48 +0800

    Revert "Revert "update""
    This reverts commit 587d4e9fe6e661458d23387e9ba2117a57681080.

--- src/darkwindstom/json/sample/ParseJsonArray.java
+++ src/darkwindstom/json/sample/ParseJsonArray.java
@@ -1,9 +1,7 @@
 package darkwindstom.json.sample;
 
 import java.util.ArrayList;
-
 import com.google.gson.Gson;
-
 import darkwindstom.json.sample.BookJsonArray.Result;
 
 public class ParseJsonArray {
@@ -17,68 +15,24 @@ public class ParseJsonArray {
 	
 		//new BookJsonArray Object
 		BookJsonArray book = new BookJsonArray(true, "sucess");
-			
+
 		//print Json formate data
-		//System.out.println(gson.toJson(book).toString());
-		
-		
-		
-		
+		System.out.println(gson.toJson(book).toString());
 
 
 		ArrayList<Result> abc = new ArrayList<Result>();
-		
-		
-		//for(int i=0; i<2; i++){
 
-			Result tmp = new Result();
-			tmp.setAuthor("abc");
-			tmp.setBookname("sdfe");
-			tmp.setPrice("123456");
-			tmp.setPurchase_ID("1234567890");
-			abc.add(tmp);
-		
-		//}
-		
-			//System.out.println(abc.size());
+		Result tmp1 = new Result("a", "b", "c", "d");			
+		abc.add(tmp1);
 			
-			
-			Result oResult[] = (Result[]) abc.toArray();
-			
-			
-			//System.out.println(oResult[0]);
-			
-			
-			
-		BookJsonArray book2 = new BookJsonArray(true, "sucess", (Result[])abc.toArray());
-	
-		
-		//System.out.println(gson.toJson(book2).toString());
-		
-	
-			ArrayList al = new ArrayList();
-			al.add("a");
-			al.add("b");
-			al.add("c");
+		Result tmp2 = new Result("e", "f", "g", "h");
+		abc.add(tmp2);
 
-			//方法一(推薦)
-			String a[] = (String[]) al.toArray(new String[0]);
+		BookJsonArray book2 = new BookJsonArray(true, "sucess", (Result[])abc.toArray(new Result[abc.size()]));
 
-			//方法二
-			//String a[] = new String[al.size()];
-			//a = al.toArray(a);
+		System.out.println(gson.toJson(book2).toString());
 
-			//測試
-
-			for (String s : a) {
-				System.out.println(s);
-			}
 			
-			
-	
 	}
-	
-	
-	
 
 }
