9ff8a75eedc8f73c121bfed9ad8e5315912a14bc
darkwindstom <darkwindstom@gmail.com>
2014-10-16 21:27:13 +0800

    Revert "Revert "Revert "update"""
    This reverts commit 39341fdfc5f6fc89bc515630ce0340180d7b0397.

--- src/darkwindstom/json/sample/ParseJsonArray.java
+++ src/darkwindstom/json/sample/ParseJsonArray.java
@@ -1,7 +1,9 @@
 package darkwindstom.json.sample;
 
 import java.util.ArrayList;
+
 import com.google.gson.Gson;
+
 import darkwindstom.json.sample.BookJsonArray.Result;
 
 public class ParseJsonArray {
@@ -15,24 +17,68 @@ public class ParseJsonArray {
 	
 		//new BookJsonArray Object
 		BookJsonArray book = new BookJsonArray(true, "sucess");
-
+			
 		//print Json formate data
-		System.out.println(gson.toJson(book).toString());
+		//System.out.println(gson.toJson(book).toString());
+		
+		
+		
+		
 
 
 		ArrayList<Result> abc = new ArrayList<Result>();
+		
+		
+		//for(int i=0; i<2; i++){
 
-		Result tmp1 = new Result("a", "b", "c", "d");			
-		abc.add(tmp1);
+			Result tmp = new Result();
+			tmp.setAuthor("abc");
+			tmp.setBookname("sdfe");
+			tmp.setPrice("123456");
+			tmp.setPurchase_ID("1234567890");
+			abc.add(tmp);
+		
+		//}
+		
+			//System.out.println(abc.size());
 			
-		Result tmp2 = new Result("e", "f", "g", "h");
-		abc.add(tmp2);
-
-		BookJsonArray book2 = new BookJsonArray(true, "sucess", (Result[])abc.toArray(new Result[abc.size()]));
-
-		System.out.println(gson.toJson(book2).toString());
-
 			
+			Result oResult[] = (Result[]) abc.toArray();
+			
+			
+			//System.out.println(oResult[0]);
+			
+			
+			
+		BookJsonArray book2 = new BookJsonArray(true, "sucess", (Result[])abc.toArray());
+	
+		
+		//System.out.println(gson.toJson(book2).toString());
+		
+	
+			ArrayList al = new ArrayList();
+			al.add("a");
+			al.add("b");
+			al.add("c");
+
+			//方法一(推薦)
+			String a[] = (String[]) al.toArray(new String[0]);
+
+			//方法二
+			//String a[] = new String[al.size()];
+			//a = al.toArray(a);
+
+			//測試
+
+			for (String s : a) {
+				System.out.println(s);
+			}
+			
+			
+	
 	}
+	
+	
+	
 
 }
